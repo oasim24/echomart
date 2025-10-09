acb3d5d5bde044287bdef60bb50f6cd0c27f0348
oasim24 <oasim547@gmmail.com>
2025-10-09 22:49:26 +0600

    oasim last update

--- app/Http/Controllers/Admin/InventoriesController.php
+++ app/Http/Controllers/Admin/InventoriesController.php
@@ -17,7 +17,17 @@ class InventoriesController extends Controller
 
     public function store(Request $request)
     {
-        dd($request->all());
+          $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'nullable',
+            'stock' => 'nullable',
+            'warehouse_id' => 'nullable',
+            'photo' => 'nullable',
+        ]);
+
+    Inventory::create($validated);
+
+    return redirect()->back()->with('success', 'Category created successfully!');
     }
 
 
@@ -28,4 +38,18 @@ class InventoriesController extends Controller
         $houses = Warehouse::all();
         return view('admin.inventories.index', compact('invents', 'houses', 'invent'));
     }
+
+
+    public function update(Request $request, $id)
+{
+    $invent = Inventory::findOrFail($id);
+    $request->validate([
+        'name'  => 'required|string|max:255',
+        'photo' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048',
+    ]);
+    $invent->name = $request->name;
+    $invent->photo = $request->photo;
+    $invent->save();
+    return redirect()->route('brands')->with('success', 'Brand updated successfully!');
+}
 }
